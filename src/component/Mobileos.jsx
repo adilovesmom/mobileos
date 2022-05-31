a85c2e1a4d781e858744f77169e853befad42623
Aditya pradhan <a1aditya.pradhan@gmail.com>
2022-06-01 01:43:25 +0530

    done

---
+++ src/component/Mobileos.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Mobileos() {
+  return (
+      <div>
+    <div>
+        <h1>Mobile Operating System</h1>
+        <ul>
+            <li>Android</li>
+            <li>Blackberry</li>
+            <li>Iphone</li>
+            <li>Windows Phone</li>
+        </ul>
+        
+    </div>
+    <div>
+        <h1>Mobile Manufacturers</h1>
+        <ul>
+            <li>Samsung</li>
+            <li>HTC</li>
+            <li>Micromax</li>
+            <li>Apple</li>
+        </ul>
+    </div>
+    </div>
+
+  )
+}
+
+export default Mobileos
